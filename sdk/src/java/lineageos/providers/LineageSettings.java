7cb2b6949753e5392d927951fe1a4725bf02d261
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-22 19:43:27 +0800

    add ios battery styles
    Change-Id: I9f904b575a97a7a92e24118357b315b6a80e8447
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -992,7 +992,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 4);
+                new InclusiveIntegerRangeValidator(0, 6);
 
         /**
          * Status bar battery %
